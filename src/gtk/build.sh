fb61044a654b873cca88cb7d4e059316e07b45e0
pedrollauer <pedro.llauer@gmail.com>
2022-05-24 07:03:25 -0300

    Arquitetura Inicial

--- src/gtk/build.sh
+++ src/gtk/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wno-format -o final main.cpp -Wno-deprecated-declarations -Wno-format-security -lm `pkg-config --cflags --libs gtkmm-3.0` -rdynamic
+g++ -lpthread -lmodbus -Wno-format -o final main.cpp -Wno-deprecated-declarations -Wno-format-security -lm `pkg-config --cflags --libs gtkmm-3.0` -rdynamic 
 
--- src/gtk/main.cpp
+++ src/gtk/main.cpp
@@ -1,6 +1,8 @@
 #include <gtk/gtk.h>
 #include <gtk/gtkx.h>
-
+#include <pthread.h>
+#include <modbus/modbus.h>
+#include <iostream>
 
 GtkWidget *window;
 GtkWidget *fixed1;
@@ -8,8 +10,65 @@ GtkWidget *btn_servo_on;
 GtkWidget *l_status;
 GtkBuilder *builder;
 
+modbus_t *ctx;
+
+void setup(){
+modbus_t *ctx = modbus_new_rtu("/dev/ttyUSB0", 9600, 'N', 8, 1);
+int modo= modbus_rtu_set_serial_mode(ctx,MODBUS_RTU_RS485);
+std::cout<<"MODO DE COMUNICAO"<<std::endl;
+if(!ctx) {
+    fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
+    exit(1);
+}
+
+if (modbus_connect(ctx) == -1) {
+    fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
+    modbus_free(ctx);
+    exit(1);
+}
+
+//Set the Modbus address of the remote slave (to 3)
+modbus_set_slave(ctx, 2);
+
+
+
+//Read 5 holding registers starting from address 10
+
+}
+
+
+void* loop(void *msg1){
+        while(true){
+
+        printf("Hey! I'm talking to the servodrive!\n");
+        printf("=)");
+
+uint16_t reg[5];// will store read registers values
+
+int num = modbus_read_registers(ctx,0x300,1, reg);
+int result;
+
+
+result=modbus_write_register(ctx,0x30c,0x3f01);
+printf("----------------------------------------->%d\n",result);
+
+
+
+if (num !=1) {// number of read registers is not the one expected
+    std::cout<<"ERRO------->"<<modbus_strerror(errno)<<std::endl;
+}
+
+std::cout<<"MITO---->"<<reg[0]<<std::endl;
+
+
+
+        }
+}
+
 
 int main(int argc,char *argv[]){
+        pthread_t t1;
+        pthread_create(&t1,NULL,loop,NULL);
         gtk_init(&argc,&argv);
         builder=gtk_builder_new_from_file("interface.glade");
         window=GTK_WIDGET(gtk_builder_get_object(builder,"window"));
@@ -22,8 +81,12 @@ int main(int argc,char *argv[]){
 
         gtk_widget_show(window);
         gtk_main();
+        pthread_join(t1,NULL);
+        modbus_close(ctx);
+        modbus_free(ctx);
         return EXIT_SUCCESS;    
 }
+
 extern "C" G_MODULE_EXPORT void on_click(GtkButton *b)
 {
     gtk_label_set_text(GTK_LABEL(l_status), "SERVO OFF");
